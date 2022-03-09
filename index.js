a02a23673c8b67b351e7ae9ec62d4134d5eb4936
vukien <vutrungkien2k@gmail.com>
2022-03-09 11:27:21 +0700

    fix currentuser undefine

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 // middleware set flash and define isUserLogin
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user || true
+    res.locals.currentUser = req.user
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
     next()
